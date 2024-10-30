3c8597bb0915140e3a204e2be9f96eb28e5ef896
Aslı Sema Gültekin (ETIYA) <asli.gultekin@etiya.com>
2024-10-30 18:15:24 +0300

    update customer-search service

--- src/app/shared/services/customer-search.service.ts
+++ src/app/shared/services/customer-search.service.ts
@@ -19,32 +19,4 @@ export class CustomerSearchService {
   }
 
 
-  searchCustomer(
-    nationalityId?: string,
-    id?: string,
-    accountNumber?: string,
-    mobilePhone?: string,
-    firstName?: string,
-    lastName?: string,
-    orderNumber?: string,
-    sortField?: string,
-    sortOrder?: string
-  ): Observable<any> {
-    let params = new HttpParams();
-
-    if (nationalityId) params = params.set('nationalityId', nationalityId);
-    if (id) params = params.set('id', id);
-    if (accountNumber) params = params.set('accountNumber', accountNumber);
-    if (mobilePhone) params = params.set('mobilePhone', mobilePhone);
-    if (firstName) params = params.set('firstName', firstName);
-    if (lastName) params = params.set('lastName', lastName);
-    if (orderNumber) params = params.set('orderNumber', orderNumber);
-    if (sortField) params = params.set('sortField', sortField);
-    if (sortOrder) params = params.set('sortOrder', sortOrder);
-
-    return this.httpClient.get<CustomerSearchResponse>(this.controllerUrl, { params });
-  }
-
-
-
 }
