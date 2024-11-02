3ea3879f24b3eb43c7845ab7c952ea62f1d2767f
mustafaceliiikel <69846017+mustafaceliiikel@users.noreply.github.com>
2024-11-02 12:58:08 +0300

    fixed customer info components

--- src/app/shared/components/customer-info/customer-info.component.ts
+++ src/app/shared/components/customer-info/customer-info.component.ts
@@ -3,20 +3,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CustomerCncinfoService } from '../../services/customer-cncinfo.service';
 import { CustomerCncInfoResponse } from '../../models/customer/customerCncInfoResponse';
+import { PopupComponent } from '../popup/popup.component';
+import { ButtonComponent } from '../button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PopupComponent, ButtonComponent],
   templateUrl: './customer-info.component.html',
-  styleUrls: ['./customer-info.component.scss']
+  styleUrls: ['./customer-info.component.scss'],
 })
 export class CustomerInfoComponent implements OnInit {
-
+  showModal: boolean = false;
   customer: CustomerCncInfoResponse = {} as CustomerCncInfoResponse;
 
-
   constructor(
+    private router: Router,
     private customerCncInfoService: CustomerCncinfoService,
     private route: ActivatedRoute // ActivatedRoute servisini ekleyin
   ) {}
@@ -30,8 +33,24 @@ export class CustomerInfoComponent implements OnInit {
   }
 
   getCustomer(id: number) {
-    this.customerCncInfoService.getCustomerById({ id }).subscribe((response) => {
-      this.customer = response;
-    });
+    this.customerCncInfoService
+      .getCustomerById({ id })
+      .subscribe((response) => {
+        this.customer = response;
+      });
+  }
+
+  handleClick() {}
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  goToCustomerCreate() {
+    this.router.navigate(['/customer-create']);
   }
 }
